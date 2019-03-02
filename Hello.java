e60441416e2ac612fe7eadf355051bf50396a887
Ashish <ashishkumar311291@gmail.com>
2019-03-02 10:07:00 +0530

    updated hello.java

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 class Hello{
 	public static void main(String[] args){
 		for(int i=0; i<=10; i++ ){
-		System.out.println("hello world... updated-1 "+i );	
+		System.out.println("hello world from ashish... updated-1 "+i );	
 		}
 		
 	}
